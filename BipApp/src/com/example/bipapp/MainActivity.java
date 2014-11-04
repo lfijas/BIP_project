5f159519f2c36cf57dacbdd4ad02cbfdc6489063
Lukasz Fijas <lukaszfijas@yahoo.com>
2014-11-04 23:42:27 +0100

    Querying database about products and displaying results

--- BipApp/src/com/example/bipapp/MainActivity.java
+++ BipApp/src/com/example/bipapp/MainActivity.java
@@ -3,22 +3,42 @@ package com.example.bipapp;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private ListView searchResults;
+	private ArrayAdapter arrayAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		new DbConnection().execute("test");
+		
+		final Button searchButton = (Button) findViewById(R.id.search_button);
+		final EditText searchText = (EditText) findViewById(R.id.search_text);
+		searchButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				new DbConnection(MainActivity.this).execute((String)searchText.getText().toString());
+			}
+		});
+		
 	}
 
 	@Override
@@ -28,37 +48,62 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	private class DbConnection extends AsyncTask<String, Void, String> {
-
-		@Override
-		protected String doInBackground(String... params) {
-			connectToDatabase();
-			return "ok";
+	private class DbConnection extends AsyncTask<String, Void, ArrayList<String>> {
+		
+		private Context mContext;
+		
+		public DbConnection(Context context) {
+			mContext = context;
 		}
 		
-		public void connectToDatabase() {
-		    try {
-		 
-		         // SET CONNECTIONSTRING
-		         Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-		         String username = "Admin";
-		         String password = "BIP_project";
-		         Connection DbConn = DriverManager.getConnection("jdbc:jtds:sqlserver://54.69.68.63:1433/BIP_project;user=" + username + ";password=" + password);
+		@Override
+		protected ArrayList<String> doInBackground(String... params) {
+			return connectToDatabase(params[0]);
+		}
+		
+		@Override
+		protected void onPostExecute(ArrayList<String> result) {
+			searchResults = (ListView) findViewById(R.id.search_result_list_view);
+			
+			String[] testArray = {"one", "two"};
+			arrayAdapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, result);
+			searchResults.setAdapter(arrayAdapter);
+		}
+		
+		public ArrayList<String> connectToDatabase(String productName) {
+		    
+			Connection conn = null;
+			ArrayList<String> resultArrayList = null;
+			
+			try {
+		        Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+		        String username = "Admin";
+		        String password = "BIP_project";
+		        conn = DriverManager.getConnection("jdbc:jtds:sqlserver://54.69.68.63:1433/BIP_project;user=" + username + ";password=" + password);
 		 
 		        Log.w("Connection","open");
-		        //Statement stmt = DbConn.createStatement();
-		        //ResultSet reset = stmt.executeQuery(" select * from users ");
+		        Statement statement = conn.createStatement();
+		        ResultSet resultSet = statement.executeQuery("SELECT product_name  FROM Product" +
+		        		" WHERE product_name like '%" + productName + "%' collate SQL_Latin1_General_CP1_CI_AI");
 		 
 
-		        //EditText num = (EditText) findViewById(R.id.displaymessage);
-		        //num.setText(reset.getString(1));
+		        resultArrayList = new ArrayList<String>();
+				try {
+					while (resultSet != null && resultSet.next()) {
+						resultArrayList.add(resultSet.getString("product_name"));
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		 
-		        DbConn.close();
+	        	conn.close();
 		 
 		        } catch (Exception e) {
 		        	Log.w("Error connection","" + e.getMessage());
 		        	e.printStackTrace();
 		        }
+			return resultArrayList;
 		}
 	}
 }
