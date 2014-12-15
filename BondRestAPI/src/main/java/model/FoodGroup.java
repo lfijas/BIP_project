e6c86b59d8bea803ca4a3060a53a0e424ae7b29d
esamota <esamota@css.edu>
2014-12-15 14:42:17 +0100

    food group resource modified

--- BondRestAPI/src/main/java/model/FoodGroup.java
+++ BondRestAPI/src/main/java/model/FoodGroup.java
@@ -2,12 +2,24 @@ package model;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.DBConnector;
 
 public class FoodGroup {
 
 	private String name;
+	
+	public static void main(String[] args) {
+		
+		List<Product> products = getProductsFromFoodGroup("Fruit");
+	
+		for(int i = 0; i < products.size(); i++) {
+	            System.out.println(products);
+	        }
+
+	}
 
 	public String getName() {
 		return name;
@@ -48,5 +60,25 @@ public class FoodGroup {
 		DBConnector.closeConnection();
 		return id;
 	}
+	
+	public static List<Product> getProductsFromFoodGroup(String name){
+		List<Product> products = new ArrayList<>();
+		DBConnector.connect();
+		
+		ResultSet result = DBConnector.query("SELECT p.barcode, p.group_id, p.product_name, p.price, p.quantity_number, p.unit, p.serving_size, p.calories, p.proteins_100, p.carbohydrates_100, p.sugar_100, p.fat_100, p.saturated_fat_100, p.fiber_100, p.cholesterol_100, p.sodium_100, p.calcium_100, p.iron_100, p.vitamin_a, p.vitamin_c FROM Product p LEFT JOIN Food_groups f on p.group_id = f.id WHERE f.group_name = '"+name+"'");
+		
+		try {
+			while(result.next()) {
+				Product product = new Product();
+				Product.setProduct(product, result);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		DBConnector.closeConnection();
+		return products;
+	}
+	
 	
 }
